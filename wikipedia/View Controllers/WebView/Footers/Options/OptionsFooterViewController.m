eff7cf7112b29ac1c04535e6c71a264602f238d9
Monte Hurd <mhurd@wikimedia.org>
2015-02-17 15:57:41 -0800

    WIP: Native component beneath webView.
    Discovered better way to make native view track web view
    with autolayout. Factored to category on UIWebView for
    easy top or bottom placement of tracking view.
    
    Made lead image use the new category.
    
    Moved article footer from html/css to native
    component tracking bottom of article.
    
    Made tracking containment use intrinsic content size
    rather than hard-coded height constraints.
    
    Change-Id: I60f1ac5b9ffbe5eb1e75c0d30926063a884019c0

---
+++ wikipedia/View Controllers/WebView/Footers/Options/OptionsFooterViewController.m
@@ -0,0 +1,185 @@
+//
+//  OptionsFooterViewController.m
+//  Wikipedia
+//
+//  Created by Monte Hurd on 2/16/15.
+//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import "OptionsFooterViewController.h"
+#import "PaddedLabel.h"
+#import "WikipediaAppUtils.h"
+#import "MWLanguageInfo.h"
+#import "WikiGlyph_Chars.h"
+#import "WikiGlyphLabel.h"
+#import "WMF_Colors.h"
+#import "Defines.h"
+#import "NSObject+ConstraintsScale.h"
+#import "NSString+FormattedAttributedString.h"
+#import "WMF_Colors.h"
+#import "UIViewController+ModalPresent.h"
+#import "LanguagesViewController.h"
+
+#pragma mark Font sizes
+
+static const CGFloat kGlyphButtonFontSize = 23.0f;
+static const CGFloat kOptionTextFontSize = 15.0f;
+static const CGFloat kOptionTextLineSpacing = 2.0f;
+
+#pragma mark Colors
+
+static const NSInteger kBaseTextColor = 0x565656;
+
+static const NSInteger kLastModGlyphBackgroundColor = 0x565656;
+static const NSInteger kLastModGlyphForgroundColor = 0xffffff;
+static const NSInteger kLastModTimestampColor = 0x565656;
+static const NSInteger kLastModUsernameColor = 0x565656;
+
+static const NSInteger kLangGlyphBackgroundColor = 0x565656;
+static const NSInteger kLangGlyphForgroundColor = 0xffffff;
+static const NSInteger kLangCountColor = 0x565656;
+
+#pragma mark Private properties
+
+@interface OptionsFooterViewController () <LanguageSelectionDelegate>
+
+@property (nonatomic, weak) IBOutlet WikiGlyphLabel *langGlyphLabel;
+@property (nonatomic, weak) IBOutlet PaddedLabel *langLabel;
+
+@property (nonatomic, weak) IBOutlet WikiGlyphLabel *lastModGlyphLabel;
+@property (nonatomic, weak) IBOutlet PaddedLabel *lastModLabel;
+
+@end
+
+@implementation OptionsFooterViewController
+
+#pragma mark Setup / view lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self roundGlyphButtonCorners];
+    
+    [self adjustConstraintsScaleForViews:
+        @[self.langGlyphLabel, self.langLabel, self.lastModGlyphLabel, self.lastModLabel]];
+}
+
+#pragma mark Style
+
+-(void)roundGlyphButtonCorners
+{
+    self.langGlyphLabel.layer.cornerRadius = self.langGlyphLabel.frame.size.width / 2.0f;
+    self.lastModGlyphLabel.layer.cornerRadius = self.langGlyphLabel.frame.size.width / 2.0f;
+    self.langGlyphLabel.clipsToBounds = YES;
+    self.lastModGlyphLabel.clipsToBounds = YES;
+}
+
+-(NSDictionary *)getOptionTextBaseAttributes
+{
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.lineSpacing = kOptionTextLineSpacing * MENUS_SCALE_MULTIPLIER;
+    return @{
+             NSFontAttributeName : [UIFont systemFontOfSize:kOptionTextFontSize],
+             NSForegroundColorAttributeName : UIColorFromRGBWithAlpha(kBaseTextColor, 1.0),
+             NSParagraphStyleAttributeName : paragraphStyle
+             };
+}
+
+#pragma mark Language option
+
+-(void)updateLanguageCount:(NSInteger)count
+{
+    self.langGlyphLabel.backgroundColor = UIColorFromRGBWithAlpha(kLangGlyphBackgroundColor, 1.0);
+
+    [self.langGlyphLabel setWikiText: WIKIGLYPH_TRANSLATE
+                               color: UIColorFromRGBWithAlpha(kLangGlyphForgroundColor, 1.0)
+                                size: kGlyphButtonFontSize * MENUS_SCALE_MULTIPLIER
+                      baselineOffset: 1.7];
+
+    self.langLabel.attributedText = [self getAttributedStringForOptionLanguagesWithCount:count];
+}
+
+-(NSAttributedString *)getAttributedStringForOptionLanguagesWithCount:(NSInteger)count
+{
+    NSString *langButtonString = [MWLocalizedString(@"language-button-text", nil) stringByReplacingOccurrencesOfString:@"%d" withString:@"$1"];
+    
+    return
+    [langButtonString attributedStringWithAttributes: [self getOptionTextBaseAttributes]
+                  substitutionStrings: @[[NSString stringWithFormat:@"%ld", (long)count]]
+               substitutionAttributes: @[@{NSForegroundColorAttributeName : UIColorFromRGBWithAlpha(kLangCountColor, 1.0)}]];
+}
+
+#pragma mark Last modified option
+
+-(void)updateLastModifiedDate:(NSDate *)date userName:(NSString *)userName
+{
+    self.lastModLabel.attributedText = [self getAttributedStringForOptionLastModifiedByUserName:userName date:date];
+
+    self.lastModGlyphLabel.backgroundColor = UIColorFromRGBWithAlpha(kLastModGlyphBackgroundColor, 1.0);
+    [self.lastModGlyphLabel setWikiText: WIKIGLYPH_PENCIL
+                                  color: UIColorFromRGBWithAlpha(kLastModGlyphForgroundColor, 1.0)
+                                   size: kGlyphButtonFontSize * MENUS_SCALE_MULTIPLIER
+                         baselineOffset: 1.7];
+}
+
+-(NSAttributedString *)getAttributedStringForOptionLastModifiedByUserName:(NSString *)userName date:(NSDate *)date
+{
+    NSString *relativeTimeStamp = [WikipediaAppUtils relativeTimestamp:date];
+    NSString *lastModString = userName ? MWLocalizedString(@"lastmodified-by-user", nil) : MWLocalizedString(@"lastmodified-by-anon", nil);
+
+    return
+    [lastModString attributedStringWithAttributes: [self getOptionTextBaseAttributes]
+                              substitutionStrings: @[relativeTimeStamp, (userName ? userName : @"")]
+                           substitutionAttributes: @[
+                                                     @{NSForegroundColorAttributeName : UIColorFromRGBWithAlpha(kLastModTimestampColor, 1.0)},
+                                                     @{NSForegroundColorAttributeName : UIColorFromRGBWithAlpha(kLastModUsernameColor, 1.0)}
+                                                     ]];
+}
+
+#pragma mark Tap gesture handling
+
+-(IBAction)historyOptionTapped:(id)sender
+{
+    [self performModalSequeWithID: @"modal_segue_show_page_history"
+                  transitionStyle: UIModalTransitionStyleCoverVertical
+                            block: nil];
+}
+
+-(IBAction)languagesOptionTapped:(id)sender
+{
+    [self performModalSequeWithID: @"modal_segue_show_languages"
+                  transitionStyle: UIModalTransitionStyleCoverVertical
+                            block: ^(LanguagesViewController *languagesVC){
+                                languagesVC.downloadLanguagesForCurrentArticle = YES;
+                                languagesVC.invokingVC = self;
+                                languagesVC.languageSelectionDelegate = self;
+                            }];
+}
+
+#pragma mark LanguageSelectionDelegate
+
+- (void)languageSelected:(NSDictionary *)langData sender:(LanguagesViewController *)sender
+{
+    MWKSite *site = [[MWKSite alloc] initWithDomain:@"wikipedia.org" language:langData[@"code"]];
+    MWKTitle *title = [site titleWithString:langData[@"*"]];
+    [NAV loadArticleWithTitle: title
+                     animated: NO
+              discoveryMethod: MWK_DISCOVERY_METHOD_SEARCH
+                   popToWebVC: YES];
+
+    [self dismissLanguagePicker];
+}
+
+-(void)dismissLanguagePicker
+{
+    [self.presentedViewController dismissViewControllerAnimated: YES
+                                                     completion: ^{}];
+}
+
+#pragma mark Memory
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
