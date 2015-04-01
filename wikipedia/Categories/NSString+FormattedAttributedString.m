5527a74317de064048dbe7d14bca89ec31794e23
Monte Hurd <mhurd@wikimedia.org>
2015-03-31 18:24:59 -0700

    Reuse of NSRegularExpression objects in string formatting method.
    Had been re-instancing regex objects in a loop.
    
    Added tests. ~65% performance increase.
    
    Change-Id: I30ddc7bd59b163b1300dbe6b0c2587337d8581b7

--- wikipedia/Categories/NSString+FormattedAttributedString.m
+++ wikipedia/Categories/NSString+FormattedAttributedString.m
@@ -8,23 +8,41 @@
 - (NSAttributedString*)attributedStringWithAttributes:(NSDictionary*)attributes
                                   substitutionStrings:(NSArray*)substitutionStrings
                                substitutionAttributes:(NSArray*)substitutionAttributes {
+    static NSLock* regexArrayProtectionLock = nil;
+    if (!regexArrayProtectionLock) {
+        regexArrayProtectionLock = [[NSLock alloc] init];
+    }
+
+    [regexArrayProtectionLock lock];
+
+    static NSMutableArray* regexArray = nil;
+    if (!regexArray) {
+        regexArray = @[].mutableCopy;
+    }
+    if (regexArray.count < substitutionStrings.count) {
+        NSInteger regexCountNeeded   = substitutionStrings.count - regexArray.count;
+        NSInteger regexCountPrevious = regexArray.count;
+        for (NSUInteger i = 0; i < regexCountNeeded; i++) {
+            NSRegularExpression* newRegex = [NSRegularExpression regularExpressionWithPattern:[NSString stringWithFormat:@"\\$%lu+", (unsigned long)i + regexCountPrevious + 1] options:0 error:nil];
+            [regexArray addObject:newRegex];
+        }
+    }
+    NSAssert(regexArray.count >= substitutionStrings.count, @"Not enough NSRegularExpression objects.");
+
+    [regexArrayProtectionLock unlock];
+
     NSMutableAttributedString* returnString =
         [[NSMutableAttributedString alloc] initWithString:self
                                                attributes:attributes];
 
     for (NSUInteger i = 0; i < substitutionStrings.count; i++) {
-        NSRegularExpression* regex =
-            [NSRegularExpression regularExpressionWithPattern:[NSString stringWithFormat:@"\\$%lu+", (unsigned long)i + 1]
-                                                      options:0
-                                                        error:nil];
-        NSArray* matches =
-            [regex matchesInString:returnString.string
-                           options:0
-                             range:NSMakeRange(0, returnString.string.length)];
+        NSArray* matches = [regexArray[i] matchesInString:returnString.string
+                                                  options:0
+                                                    range:NSMakeRange(0, returnString.string.length)];
 
-        for (NSTextCheckingResult* checkingResult in [matches reverseObjectEnumerator]) {
-            [returnString setAttributes:substitutionAttributes[i] range:checkingResult.range];
-            [returnString replaceCharactersInRange:checkingResult.range withString:substitutionStrings[i]];
+        for (NSTextCheckingResult* match in [matches reverseObjectEnumerator]) {
+            [returnString setAttributes:substitutionAttributes[i] range:match.range];
+            [returnString replaceCharactersInRange:match.range withString:substitutionStrings[i]];
         }
     }
     return returnString;
