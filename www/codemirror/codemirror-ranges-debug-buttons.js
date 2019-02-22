8821c787661928896b16c3763ae0bf5734081c76
montehurd <mhurd@wikimedia.org>
2019-02-21 17:19:13 -0800

    WIP 2

---
+++ www/codemirror/codemirror-ranges-debug-buttons.js
@@ -0,0 +1,95 @@
+// uncomment call to 'showRangeDebuggingButtons()' at bottom to use
+const showRangeDebuggingButtons = () => {
+  
+  var markupItems = []
+  var currentItemIndex = 0
+
+  var highlightHandle = null
+  var useOuter = true
+
+  const addButton = (title, tapClosure) => {
+    const button = document.createElement('button')
+    button.innerHTML = title
+    document.body.insertBefore(button, document.body.firstChild)
+    button.addEventListener ('click', tapClosure)
+  }
+
+  const clearItems = () => {
+    markupItems = []
+  }
+
+  const clearHighlightHandle = () => {
+    if (highlightHandle) {
+      highlightHandle.clear()
+    }
+    highlightHandle = null
+  }
+
+  const addTestingButtons = () => {
+    addButton('reset', () => {
+      reset()
+      currentItemIndex = 0
+      console.log('reset')    
+    })
+
+    addButton('>', () => {
+      clearHighlightHandle()
+      currentItemIndex = currentItemIndex + 1
+      if (currentItemIndex > (markupItems.length - 1)) {
+        currentItemIndex = markupItems.length - 1
+      }
+      flickerTextForMarkupItemAtIndex(currentItemIndex)    
+      console.log('next')    
+    })
+
+    addButton('<', () => {
+      clearHighlightHandle()
+      currentItemIndex = currentItemIndex - 1
+      if (currentItemIndex < 0) {
+        currentItemIndex = 0
+      }
+      flickerTextForMarkupItemAtIndex(currentItemIndex)    
+      console.log('prev')    
+    })
+
+    const reset = () => {
+      clearHighlightHandle()
+      clearItems()    
+    }
+
+    const kickoff = () => {
+      reset()
+
+      markupItems = markupItemsForLine(editor.getCursor().line)
+      // markupItems = tagMarkupItemsForLine(editor.getCursor().line)
+
+      flickerTextForMarkupItemAtIndex(currentItemIndex)
+    }
+
+    addButton('test outer', () => {
+      useOuter = true
+      kickoff()
+    })
+
+    addButton('test inner', () => {
+      useOuter = false
+      kickoff()
+    })
+  }
+
+  const flickerTextForMarkupItemAtIndex = (index) => {
+    const line = editor.getCursor().line
+    const markupItem = markupItems[index]
+    const range = useOuter ? markupItem.outer : markupItem.inner
+
+    clearHighlightHandle()
+    highlightHandle = editor.markText({line: line, ch: range.start}, {line: line, ch: range.end}, {
+      className: 'testOuter'
+    })
+  }
+
+  // could inject testing text here too
+  setTimeout(addTestingButtons, 2000)
+}
+
+showRangeDebuggingButtons()
