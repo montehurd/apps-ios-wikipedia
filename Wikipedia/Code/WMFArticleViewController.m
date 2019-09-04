2dc4436d103e73480b8862ab733158b021a707de
montehurd <mhurd@wikimedia.org>
2019-09-03 17:52:42 -0700

    consolidate toc highlight update logic, add `force` parameter

--- Wikipedia/Code/WMFArticleViewController.m
+++ Wikipedia/Code/WMFArticleViewController.m
@@ -1620,7 +1620,25 @@ NSString *const WMFEditPublishedNotification = @"WMFEditPublishedNotification";
     }];
 }
 
-- (void)updateTableOfContentsHighlightWithScrollView:(UIScrollView *)scrollView {
+static const CGFloat WMFArticleViewControllerTableOfContentsSectionUpdateScrollDistance = 15;
+
+- (void)updateTOCHighlightIfNecessaryWithScrollView:(UIScrollView *)scrollView force:(BOOL)force {
+    if (!self.tableOfContentsViewController.viewIfLoaded) {
+        return;
+    }
+    if (!self.isUpdateTableOfContentsSectionOnScrollEnabled) {
+        return;
+    }
+    if (!force) {
+        BOOL sectionUpdateScrollDistanceExceeded = ABS(self.previousContentOffsetYForTOCUpdate - scrollView.contentOffset.y) > WMFArticleViewControllerTableOfContentsSectionUpdateScrollDistance;
+        if (!sectionUpdateScrollDistanceExceeded) {
+            return;
+        }
+        if (!(scrollView.isTracking || scrollView.isDragging || scrollView.isDecelerating || self.restoreScrollPosition)) {
+            return;
+        }
+    }
+
     self.sectionToRestoreScrollOffset = nil;
     @weakify(self);
 
@@ -1640,6 +1658,7 @@ NSString *const WMFEditPublishedNotification = @"WMFEditPublishedNotification";
     }];
 
     self.previousContentOffsetYForTOCUpdate = scrollView.contentOffset.y;
+    self.restoreScrollPosition = NO;
 }
 
 - (void)webViewController:(WebViewController *)controller scrollViewWillBeginDragging:(UIScrollView *)scrollView {
@@ -1649,15 +1668,8 @@ NSString *const WMFEditPublishedNotification = @"WMFEditPublishedNotification";
     }
 }
 
-static const CGFloat WMFArticleViewControllerTableOfContentsSectionUpdateScrollDistance = 15;
-
 - (void)webViewController:(WebViewController *)controller scrollViewDidScroll:(UIScrollView *)scrollView {
-
-    if (self.isUpdateTableOfContentsSectionOnScrollEnabled && (scrollView.isTracking || scrollView.isDragging || scrollView.isDecelerating || (self.restoreScrollPosition && self.tableOfContentsViewController.viewIfLoaded != nil)) && ABS(self.previousContentOffsetYForTOCUpdate - scrollView.contentOffset.y) > WMFArticleViewControllerTableOfContentsSectionUpdateScrollDistance) {
-        [self updateTableOfContentsHighlightWithScrollView:scrollView];
-        self.restoreScrollPosition = NO;
-    }
-
+    [self updateTOCHighlightIfNecessaryWithScrollView:scrollView force:false];
     [self.navigationBarHider scrollViewDidScroll:scrollView];
 }
 
@@ -1690,9 +1702,7 @@ static const CGFloat WMFArticleViewControllerTableOfContentsSectionUpdateScrollD
 }
 
 - (void)webViewController:(WebViewController *)controller scrollViewDidScrollToTop:(UIScrollView *)scrollView {
-    if (self.isUpdateTableOfContentsSectionOnScrollEnabled) {
-        [self updateTableOfContentsHighlightWithScrollView:scrollView];
-    }
+    [self updateTOCHighlightIfNecessaryWithScrollView:scrollView force:true];
     [self.navigationBarHider scrollViewDidScrollToTop:scrollView];
 }
 
