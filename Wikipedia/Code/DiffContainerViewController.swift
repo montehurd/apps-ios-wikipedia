1460e6f6bbb6edf141a6644da12fe7f721769b70
montehurd <mhurd@wikimedia.org>
2019-11-05 14:03:39 -0800

    WIP

--- Wikipedia/Code/DiffContainerViewController.swift
+++ Wikipedia/Code/DiffContainerViewController.swift
@@ -13,7 +13,7 @@ protocol DiffRevisionRetrieving: class {
     func retrieveNextRevision(with sourceRevision: WMFPageHistoryRevision) -> WMFPageHistoryRevision?
 }
 
-class DiffContainerViewController: ViewController {
+class DiffContainerViewController: ViewController, HintPresenting {
     
     private var containerViewModel: DiffContainerViewModel
     private var headerExtendedView: DiffHeaderExtendedView?
@@ -34,6 +34,8 @@ class DiffContainerViewController: ViewController {
         progressController.delay = 0.0
         return progressController
     }()
+
+    var hintController: HintController?
     
     init(articleTitle: String, siteURL: URL, type: DiffContainerViewModel.DiffType, fromModel: WMFPageHistoryRevision?, toModel: WMFPageHistoryRevision, theme: Theme, diffController: DiffController? = nil, revisionDelegate: DiffRevisionRetrieving?) {
         self.type = type
@@ -80,8 +82,8 @@ class DiffContainerViewController: ViewController {
         
     }
     
-    @objc func tappedDown() {
-        
+    @objc func arrowDownButtonTapped(_ : UIBarButtonItem) {
+
         guard let fromModel = fromModel else {
             assertionFailure("fromModel needs to be populated at this point before user attempts to go further back in history")
             return
@@ -95,7 +97,7 @@ class DiffContainerViewController: ViewController {
         
     }
     
-    @objc func tappedUp() {
+    @objc func arrowUpButtonTapped(_ : UIBarButtonItem) {
         
         //note because we aren't filtering in History yet, PageHistoryViewController should always be able to tell us the next revision. If filtering is implemented this method will fail, and we will need to have DiffContainerViewController know how to handle a situation where fromModel is populated but toModel is not (that is, hide header & list, fetch next toModel & diff, then show header & list)
         guard let nextRevision = revisionDelegate?.retrieveNextRevision(with: toModel) else {
@@ -106,7 +108,94 @@ class DiffContainerViewController: ViewController {
         let singleDiffVC = DiffContainerViewController(articleTitle: articleTitle, siteURL: siteURL, type: .single(byteDifference: nextRevision.revisionSize), fromModel: toModel, toModel: nextRevision, theme: theme, revisionDelegate: revisionDelegate)
         wmf_push(singleDiffVC, animated: true)
     }
+
+    lazy var arrowDownButton: UIBarButtonItem = {
+        let item = UIBarButtonItem(image: UIImage(named:"chevron-down"), style: .plain, target: self, action: #selector(arrowDownButtonTapped))
+        guard let fromModel = fromModel, let previousRevision = revisionDelegate?.retrievePreviousRevision(with: fromModel) else {
+            item.isEnabled = false
+            return item
+        }
+        return item
+    }()
+
+    lazy var arrowUpButton: UIBarButtonItem = {
+        let item = UIBarButtonItem(image: UIImage(named:"chevron-up"), style: .plain, target: self, action: #selector(arrowUpButtonTapped))
+        guard let nextRevision = revisionDelegate?.retrieveNextRevision(with: toModel) else {
+            item.isEnabled = false
+            return item
+        }
+        return item
+    }()
+
+    lazy var shareButton: UIBarButtonItem = {
+        return UIBarButtonItem(image: UIImage(named:"share"), style: .plain, target: self, action: #selector(shareButtonTapped))
+    }()
+
+    lazy var smileButton: UIBarButtonItem = {
+        return UIBarButtonItem(image: UIImage(named:"diff-smile"), style: .plain, target: self, action: #selector(smileButtonTapped))
+    }()
+
+    lazy var smileButtonFilled: UIBarButtonItem = {
+        return UIBarButtonItem(image: UIImage(named:"diff-smile-filled"), style: .plain, target: self, action: #selector(filledSmileButtonTapped))
+    }()
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isToolbarHidden = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        switch type {
+        case .single:
+            self.navigationController?.isToolbarHidden = false
+            updateToolbarItems(thankButton: smileButton)
+            break
+        case .compare:
+            break
+        }
+    }
+
+    private func updateToolbarItems(thankButton: UIBarButtonItem) {
+        self.toolbarItems = [
+            UIBarButtonItem.wmf_barButtonItem(ofFixedWidth: 15),
+            arrowUpButton,
+            UIBarButtonItem.wmf_barButtonItem(ofFixedWidth: 10),
+            arrowDownButton,
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            thankButton,
+            UIBarButtonItem.wmf_barButtonItem(ofFixedWidth: 15),
+            shareButton,
+            UIBarButtonItem.wmf_barButtonItem(ofFixedWidth: 15)
+        ]
+    }
+
+    @objc func filledSmileButtonTapped(_ : UIBarButtonItem) {
+        self.show(hintViewController: AuthorAlreadyThankedHintVC())
+    }
+    
+    @objc func smileButtonTapped(_ : UIBarButtonItem) {
+        guard !toModel.isAnon else {
+            self.show(hintViewController: AnonymousUsersCannotBeThankedHintVC())
+            return
+        }
+        guard WMFAuthenticationManager.sharedInstance.isLoggedIn else {
+            wmf_showLoginOrCreateAccountToThankRevisionAuthorPanel(theme: theme, dismissHandler: nil, loginSuccessCompletion: {}, loginDismissedCompletion: nil)
+            return
+        }
+        wmf_showThankRevisionAuthorPanel(theme: theme, sendThanksHandler: {_ in
+            self.dismiss(animated: true, completion: {
+                self.thankRevisionAuthor()
+            })
+        })
+    }
+
+    @objc func shareButtonTapped(_ : UIBarButtonItem) {
+        // TODO: wire this up!
+        print("share time!")
+    }
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
@@ -143,6 +232,10 @@ class DiffContainerViewController: ViewController {
         super.apply(theme: theme)
         
         view.backgroundColor = theme.colors.midBackground
+        
+        smileButton.tintColor = toModel.isAnon ? .gray : theme.colors.link
+        smileButtonFilled.tintColor = theme.colors.link
+        shareButton.tintColor = theme.colors.link
         
         headerTitleView?.apply(theme: theme)
         headerExtendedView?.apply(theme: theme)
@@ -206,6 +299,43 @@ private extension DiffContainerViewController {
                         case .failure:
                             break
                         }
+                    }
+                }
+            }
+        case .compare:
+            break
+        }
+    }
+
+    private func show(hintViewController: HintViewController){
+        let showHint = {
+            self.hintController = HintController(hintViewController: hintViewController)
+            self.hintController?.toggle(presenter: self, context: nil, theme: self.theme)
+            self.hintController?.setHintHidden(false)
+        }
+        if let hintController = self.hintController {
+            hintController.setHintHidden(true) {
+                showHint()
+            }
+        } else {
+            showHint()
+        }
+    }
+    
+    func thankRevisionAuthor() {
+        switch type {
+        case .single:
+            diffController.thankRevisionAuthor(toRevisionId: toModel.revisionID) { [weak self] (result) in
+                guard let self = self else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let result):
+                        self.show(hintViewController: RevisionAuthorThankedHintVC(recipient: result.recipient))
+                        self.updateToolbarItems(thankButton: self.smileButtonFilled)
+                    case .failure(let error as NSError):
+                        self.show(hintViewController: RevisionAuthorThanksErrorHintVC(error: error))
                     }
                 }
             }
@@ -489,4 +619,54 @@ extension DiffContainerViewController: DiffHeaderActionDelegate {
     }
     
     
+}
+
+class AuthorAlreadyThankedHintVC: HintViewController {
+    override func configureSubviews() {
+        viewType = .warning
+        warningLabel.text = WMFLocalizedString("diff-thanks-sent-already", value: "You’ve already sent a ‘Thanks’ for this edit", comment: "Message indicating thanks was already sent")
+        warningSublabel.text = WMFLocalizedString("diff-thanks-sent-cannot-unsend", value: "Thanks cannot be unsent", comment: "Message indicating thanks cannot be unsent")
+    }
+}
+
+class AnonymousUsersCannotBeThankedHintVC: HintViewController {
+    override func configureSubviews() {
+        viewType = .warning
+        warningLabel.text = WMFLocalizedString("diff-thanks-anonymous-no-thanks", value: "Anonymous users cannot be thanked", comment: "Message indicating anonymous users cannot be thanked")
+        warningSublabel.text = nil
+    }
+}
+
+class RevisionAuthorThankedHintVC: HintViewController {
+    var recipient: String
+    init(recipient: String) {
+        self.recipient = recipient
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func configureSubviews() {
+        viewType = .default
+        let thanksMessage = WMFLocalizedString("diff-thanks-sent", value: "Your 'Thanks' was set to %1$@", comment: "Message indicating thanks was sent. Parameters:\n* %1$@ - name of user who was thanked")
+        let thanksMessageWithRecipient = String.localizedStringWithFormat(thanksMessage, recipient)
+        defaultImageView.image = UIImage(named: "selected")
+        defaultLabel.text = thanksMessageWithRecipient
+    }
+}
+
+class RevisionAuthorThanksErrorHintVC: HintViewController {
+    var error: Error
+    init(error: Error) {
+        self.error = error
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func configureSubviews() {
+        viewType = .warning
+        warningLabel.text = (error as NSError).alertMessage()
+        warningSublabel.text = nil
+    }
 }
