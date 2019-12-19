72a172f37a6816a1f1ad9a8ff0a52b8e67853054
montehurd <mhurd@wikimedia.org>
2019-12-18 20:19:09 -0800

    clean up converter

--- Wikipedia/Code/WMFArticleJSONCompilationHelper+Mobileview.swift
+++ Wikipedia/Code/WMFArticleJSONCompilationHelper+Mobileview.swift
@@ -1,6 +1,6 @@
 
-extension MWKSection {
-    fileprivate func mobileViewDict() -> [String: Any?] {
+fileprivate extension MWKSection {
+    func mobileViewDict() -> [String: Any?] {
         var dict: [String: Any?] = [:]
         dict["toclevel"] = toclevel
         dict["level"] = level?.stringValue
@@ -9,10 +9,80 @@ extension MWKSection {
         dict["index"] = index
         dict["anchor"] = anchor
         dict["id"] = sectionId
-        dict["text"] = text // stringByReplacingImageURLsWithAppSchemeURLs(inHTMLString: text ?? "", withBaseURL: baseURL, targetImageWidth: imageWidth)
+        dict["text"] = "text" // stringByReplacingImageURLsWithAppSchemeURLs(inHTMLString: text ?? "", withBaseURL: baseURL, targetImageWidth: imageWidth)
         dict["fromtitle"] = fromURL?.wmf_titleWithUnderscores
         return dict
     }
+}
+
+fileprivate extension MWKArticle {
+    func mobileViewLastModified() -> String? {
+        if let lastModifiedDate = lastmodified {
+            return iso8601DateString(lastModifiedDate)
+        }
+        return nil
+    }
+    func mobileViewLastModifiedBy() -> [String: String]? {
+        if let lastmodifiedby = lastmodifiedby {
+            return [
+                "name": lastmodifiedby.name ?? "",
+                "gender": lastmodifiedby.gender ?? ""
+            ]
+        }
+        return nil
+    }
+    func mobileViewPageProps() -> [String: String]? {
+        if let wikidataId = wikidataId {
+            return [
+                "wikibase_item": wikidataId
+            ]
+        }
+        return nil
+    }
+    func mobileViewDescriptionSource() -> String? {
+        switch descriptionSource {
+        case .local:
+            return "local"
+        case .central:
+            return "central"
+        default:
+            // should default use "local" too?
+            return nil
+        }
+    }
+    func mobileViewImage(size: CGSize) -> [String: Any]? {
+        if let imgName = image?.canonicalFilename() {
+            return [
+                "file": imgName,
+                "width": size.width,
+                "height": size.height
+            ]
+        }
+        return nil
+    }
+    func mobileViewThumbnail() -> [String: Any]? {
+        if let thumbnailSourceURL = imageURL /*article.thumbnail?.sourceURL.absoluteString*/ {
+            return [
+                "url": thumbnailSourceURL
+                // Can't seem to find the original thumb "width" and "height" to match that seen in the orig mobileview - did we not save/model these?
+            ]
+        }
+        return nil
+    }
+    func mobileViewProtection() -> [String: Any]? {
+        if let protection = protection {
+            var protectionDict:[String: Any] = [:]
+            for protectedAction in protection.protectedActions() {
+                guard let actionString = protectedAction as? String else {
+                    continue
+                }
+                protectionDict[actionString] = protection.allowedGroups(forAction: actionString)
+            }
+            return protectionDict
+        }
+        return nil
+    }
+    // ^ switch these to use guard
 }
 
 extension WMFArticleJSONCompilationHelper {
@@ -36,74 +106,20 @@ extension WMFArticleJSONCompilationHelper {
         var mvDict: [String: Any] = [:]
         
         mvDict["ns"] = article.ns
-        if let lastModifiedDate = article.lastmodified {
-            mvDict["lastmodified"] = article.iso8601DateString(lastModifiedDate)
-        }
-        if let lastmodifiedby = article.lastmodifiedby {
-            let lastmodifiedbyDict:[String: String] = [
-                "name": lastmodifiedby.name ?? "",
-                "gender": lastmodifiedby.gender ?? ""
-            ]
-            mvDict["lastmodifiedby"] = lastmodifiedbyDict
-        }
-        
+        mvDict["lastmodified"] = article.mobileViewLastModified()
+        mvDict["lastmodifiedby"] = article.mobileViewLastModifiedBy()
         mvDict["revision"] = article.revisionId
         mvDict["languagecount"] = article.languagecount
         mvDict["displaytitle"] = article.displaytitle
         mvDict["id"] = article.articleId
-        
-        if let wikidataId = article.wikidataId {
-            let pagePropsDict:[String: String] = [
-                "wikibase_item": wikidataId
-            ]
-            mvDict["pageprops"] = pagePropsDict
-        }
-        
+        mvDict["pageprops"] = article.mobileViewPageProps()
         mvDict["description"] = article.entityDescription
-        
-        switch article.descriptionSource {
-        case .local:
-            mvDict["descriptionsource"] = "local"
-        case .central:
-            mvDict["descriptionsource"] = "central"
-        default:
-            // should default use "local" too?
-            break
-        }
-        
-        mvDict["sections"] = sections.map { (section) -> [String: Any?] in
-            section.mobileViewDict()
-        }
-        
+        mvDict["descriptionsource"] = article.mobileViewDescriptionSource()
+        mvDict["sections"] = sections.map { $0.mobileViewDict() }
         mvDict["editable"] = article.editable
-
-        if let imgName = article.image?.canonicalFilename() {
-            let imageDict:[String: Any] = [
-                "file": imgName,
-                "width": imageSize.width,
-                "height": imageSize.height
-            ]
-            mvDict["image"] = imageDict
-        }
-
-        if let thumbnailSourceURL = article.imageURL /*article.thumbnail?.sourceURL.absoluteString*/ {
-            let thumbnailDict:[String: Any] = [
-                "url": thumbnailSourceURL
-                // Can't seem to find the original thumb "width" and "height" to match that seen in the orig mobileview - did we not save/model these?
-            ]
-            mvDict["thumb"] = thumbnailDict
-        }
-
-        if let protection = article.protection {
-            var protectionDict:[String: Any] = [:]
-            for protectedAction in protection.protectedActions() {
-                guard let actionString = protectedAction as? String else {
-                    continue
-                }
-                protectionDict[actionString] = protection.allowedGroups(forAction: actionString)
-            }
-            mvDict["protection"] = protectionDict
-        }
+        mvDict["image"] = article.mobileViewImage(size: imageSize)
+        mvDict["thumb"] = article.mobileViewThumbnail()
+        mvDict["protection"] = article.mobileViewProtection()
         
         return try? JSONSerialization.data(withJSONObject: ["mobileview": mvDict], options: [.prettyPrinted])
     }
